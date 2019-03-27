c93a74bb67c7707e770cf6b7e1d91c6172044edb
Nicole W <nicoleewhitee@gmail.com>
2019-03-26 18:05:46 -0700

    server set up

--- server.js
+++ server.js
@@ -1,3 +1,16 @@
-require('dotenv').config();
+require("dotenv").config();
 
+var methodOverride = require("method-override")
 var express = require("express")
+
+//setting up the port
+var PORT = process.env.PORT || 8080;
+
+var app = express();
+
+//setting up static content for the app using the public directory
+app.use(express.static("public"));
+
+//express middleware body parser
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
