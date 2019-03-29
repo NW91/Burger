5e64000902bd616dc78af6899eedfb9b52006595
Nicole W <nicoleewhitee@gmail.com>
2019-03-29 16:26:54 -0700

    html hooked up

--- config/orm.js
+++ config/orm.js
@@ -1,6 +1,14 @@
 //imports MySql connection
 var connection = require("../config/connection.js")
 
+var orm = {
+    all: function(tableInput, cb){
+        connection.query('SELECT * FROM '+tableInput+';', function(err, result) {
+            if(err) throw err;
+            cb(result)
+        })
+    }
+}
 
 
 //exports the orm object for the model
