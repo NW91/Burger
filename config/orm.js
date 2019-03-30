bde77dfa65082cc8c13ed01256a0e41560e020a8
Nicole W <nicoleewhitee@gmail.com>
2019-03-29 17:20:05 -0700

    updated orm

--- config/orm.js
+++ config/orm.js
@@ -7,8 +7,15 @@ var orm = {
             if(err) throw err;
             cb(result)
         })
+    },
+
+    update: function(tableInput, condition, cb){
+        connection.query('UPDATE '+tableInput+' SET devoured=true WHERE id='+condition+';', function(err, result){
+            if(err) throw err;
+            cb(result);
+            })
+        } 
     }
-}
 
 
 //exports the orm object for the model
