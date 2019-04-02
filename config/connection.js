9ed1f380dddd6f7370104d7f4ee7d80fe61d40bb
Nicole W <nicoleewhitee@gmail.com>
2019-04-02 12:53:07 -0700

    added heroku changes

--- config/connection.js
+++ config/connection.js
@@ -6,14 +6,19 @@ var keys = require('../config/keys.js');
 
 // Set up MySQL connection.
 var mysql = require("mysql");
+var connection;
 
-var connection = mysql.createConnection({
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+ connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
   user: "root",
   password: keys.secret,
   database: "burgers_db"
-});
+  });
+};
 
 // Make connection.
 connection.connect(function(err) {
